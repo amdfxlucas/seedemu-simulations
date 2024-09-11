502704bed74c195e35d58d70d0f402e2435e21af
amdfxlucas <saculolissat@gmx.de>
2024-09-11 20:55:06 +0200

    set number of replicas to zero for all services and change all project names to 'tiny_test'

--- start_manager.sh
+++ start_manager.sh
@@ -2,5 +2,24 @@
 
 
 
-docker stack deploy -c ./ix/docker-compose.yml  tiny_test
+#docker stack deploy -c ./ix/docker-compose.yml  tiny_test
+#docker compose -f ./ix/docker-compose.yml -f ./client/docker-compose.yml up
 
+# no services get created here, since 'replicas: ' is set to zero for all of them
+# So this will only create the overlay networks
+docker stack deploy -c ./6/_docker-compose.yml \
+-c ./7/_docker-compose.yml \
+-c ./8/_docker-compose.yml \
+-c ./9/_docker-compose.yml \
+-c ./10/_docker-compose.yml \
+-c ./11/_docker-compose.yml \
+-c ./12/_docker-compose.yml \
+-c ./13/_docker-compose.yml \
+-c ./14/_docker-compose.yml \
+-c ./15/_docker-compose.yml \
+-c ./ix/docker-compose.yml  \
+-c ./2/_docker-compose.yml \
+-c ./3/_docker-compose.yml \
+-c ./4/_docker-compose.yml  \
+-c ./5/_docker-compose.yml  \
+--with-registry-auth --resolve-image=always  tiny_test
