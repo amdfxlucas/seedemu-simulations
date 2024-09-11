e70d03fe52d86ebc3b27d13c2cff6014151c4d39
amdfxlucas <saculolissat@gmx.de>
2024-09-11 21:06:20 +0200

    wip

--- start_manager.sh
+++ start_manager.sh
@@ -7,19 +7,19 @@
 
 # no services get created here, since 'replicas: ' is set to zero for all of them
 # So this will only create the overlay networks
-docker stack deploy -c ./6/_docker-compose.yml \
--c ./7/_docker-compose.yml \
--c ./8/_docker-compose.yml \
--c ./9/_docker-compose.yml \
--c ./10/_docker-compose.yml \
--c ./11/_docker-compose.yml \
--c ./12/_docker-compose.yml \
--c ./13/_docker-compose.yml \
--c ./14/_docker-compose.yml \
--c ./15/_docker-compose.yml \
+docker stack deploy -c ./6/docker-compose.yml \
+-c ./7/docker-compose.yml \
+-c ./8/docker-compose.yml \
+-c ./9/docker-compose.yml \
+-c ./10/docker-compose.yml \
+-c ./11/docker-compose.yml \
+-c ./12/docker-compose.yml \
+-c ./13/docker-compose.yml \
+-c ./14/docker-compose.yml \
+-c ./15/docker-compose.yml \
 -c ./ix/docker-compose.yml  \
--c ./2/_docker-compose.yml \
--c ./3/_docker-compose.yml \
--c ./4/_docker-compose.yml  \
--c ./5/_docker-compose.yml  \
+-c ./2/docker-compose.yml \
+-c ./3/docker-compose.yml \
+-c ./4/docker-compose.yml  \
+-c ./5/docker-compose.yml  \
 --with-registry-auth --resolve-image=always  tiny_test
